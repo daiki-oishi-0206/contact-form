50b496c7fd561d3461a1f66d0652f67102864e35
oishi daiki <kanadai1122@icloud.com>
2026-06-05 04:44:54 +0900

    confirm画面完了

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -6,58 +6,58 @@
 
 @section('content')
 <main class="main">
-    <div class="main__inner">
-        <h1 class="main__title">confirm</h1>
+    <div class="main-inner">
+        <h1 class="main-title">confirm</h1>
         <form action="/store" method="post">
             @csrf
-            <table class="confirm_table">
+            <table class="confirm-table">
 
-                <tr class="confirm_row">
-                    <th class="confirm_header">お名前</th>
-                    <td class="confirm_data">大石 大樹</td>
+                <tr class="confirm-row">
+                    <th class="confirm-header">お名前</th>
+                    <td class="confirm-data">大石 大樹</td>
                 </tr>
 
-                <tr class="confirm_row">
-                    <th class="confirm_header">性別</th>
-                    <td class="confirm_data">男</td>
+                <tr class="confirm-row">
+                    <th class="confirm-header">性別</th>
+                    <td class="confirm-data">男</td>
                 </tr>
 
-                <tr class="confirm_row">
-                    <th class="confirm_header">メールアドレス</th>
-                    <td class="confirm_data">abc</td>
+                <tr class="confirm-row">
+                    <th class="confirm-header">メールアドレス</th>
+                    <td class="confirm-data">abc</td>
                 </tr>
 
-                <tr class="confirm_row">
-                    <th class="confirm_header">電話番号</th>
-                    <td class="confirm_data">00000000000</td>
+                <tr class="confirm-row">
+                    <th class="confirm-header">電話番号</th>
+                    <td class="confirm-data">00000000000</td>
                 </tr>
 
-                <tr class="confirm_row">
-                    <th class="confirm_header">住所</th>
-                    <td class="confirm_data">地球</td>
+                <tr class="confirm-row">
+                    <th class="confirm-header">住所</th>
+                    <td class="confirm-data">地球</td>
                 </tr>
 
-                <tr class="confirm_row">
-                    <th class="confirm_header">建物名</th>
-                    <td class="confirm_data">箱の中</td>
+                <tr class="confirm-row">
+                    <th class="confirm-header">建物名</th>
+                    <td class="confirm-data">箱の中</td>
                 </tr>
 
-                <tr class="confirm_row">
-                    <th class="confirm_header">お問い合わせの種類</th>
-                    <td class="confirm_data">なんだろうね</td>
+                <tr class="confirm-row">
+                    <th class="confirm-header">お問い合わせの種類</th>
+                    <td class="confirm-data">なんだろうね</td>
                 </tr>
 
-                <tr class="confirm_row">
-                    <th class="confirm_header">お問い合わせ内容</th>
-                    <td class="confirm_data">特に無し</td>
+                <tr class="confirm-row">
+                    <th class="confirm-header">お問い合わせ内容</th>
+                    <td class="confirm-data">特に無し</td>
                 </tr>
 
             </table>
 
 
-                <div class="confirm_buttons">
-                    <button class="confirm_submit" type="submit">送信</button>
-                    <a href="/" class="confirm_back">修正</a>
+                <div class="confirm-buttons">
+                    <button class="confirm-submit" type="submit">送信</button>
+                    <a href="/" class="confirm-back">修正</a>
                 </div>
         </form>
                 @endsection
